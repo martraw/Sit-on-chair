f523bf7838f3627d30e1779709996de59fcbd8f2
martraw <trawinski1@wp.pl>
2017-06-03 19:13:18 +0200

    Add selection feature to product price calculator

--- js/script.js
+++ js/script.js
@@ -63,14 +63,57 @@ var dropdownArrow = document.querySelectorAll(".list_arrow");
 var list_model = document.querySelectorAll(".list_panel")[0];
 var list_colors = document.querySelectorAll(".list_panel")[1];
 var list_material = document.querySelectorAll(".list_panel")[2];
+var transportTickBox = document.querySelector("#transport");
 
-console.log(list_model);
+console.log(list_model.children[1]);
 
 for (var i = 0; i < dropdownArrow.length; i++) {
   dropdownArrow[i].addEventListener("click", function(){
     this.nextElementSibling.classList.toggle("visible");
-  })
+  });
 }
 
+// for (var i = 0; i < list_model.children.length; i++) {
+//   list_model.children[i].addEventListener("click", function(){
+//     document.querySelector(".panel_left").children[0].innerHTML = "Chair " + this.innerHTML;
+//
+//     document.querySelector(".panel_right").children[0].innerHTML =  this.dataset.price + " zł";
+//
+//     this.parentNode.classList.toggle("visible");
+//   });
+// }
+// //
+// for (var i = 0; i < list_colors.children.length; i++) {
+//   list_colors.children[i].addEventListener("click", function(){
+//     document.querySelector(".summary_panel.inline .panel_left").children[1].innerHTML = this.innerHTML;
+//
+//     document.querySelector(".summary_panel.inline .panel_right").children[1].innerHTML =  this.dataset.price + " zł";
+//
+//     this.parentNode.classList.toggle("visible");
+//   });
+// }
 
+var selection = function(list, destination){
+
+  for (var i = 0; i < list.children.length; i++) {
+    list.children[i].addEventListener("click", function(){
+      document.querySelector(".panel_left " + "."+destination+"").innerHTML = this.innerHTML;
+
+      document.querySelector(".panel_right " + "."+destination+"").innerHTML = this.dataset.price + " zł";
+      this.parentNode.classList.toggle("visible");
+
+      console.log(document.querySelector("#transport").checked);
+      if (transportTickBox.checked) {
+        document.querySelector(".panel_left .transport").innerHTML = "Transport";
+
+        document.querySelector(".panel_right .transport").innerHTML = transportTickBox.dataset.transportPrice + " zł";
+      }
+    });
+
+  }
+};
+
+selection(list_model, "title");
+selection(list_colors, "color");
+selection(list_material, "pattern");
 });//Koniec
